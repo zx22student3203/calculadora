9c54cbef933c0501edfb83352cba3fbc0487ebab
zx22student3203 <145435090+zx22student3203@users.noreply.github.com>
2023-09-26 13:22:52 +0200

    Agredadas las funciones de resta

--- sumRestMult.py
+++ sumRestMult.py
@@ -10,3 +10,12 @@ if __name__ == "__main__":
     num2 = float(input("Ingrese el segundo número: "))
     resultado = sumar(num1, num2)
     print("La suma es:", resultado)
+
+def restar(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    resultado = restar(num1, num2)
+    print("La resta es:", resultado)
