b959041a659c819684b41db72e39def05d33da9b
zx22student3203 <145435090+zx22student3203@users.noreply.github.com>
2023-10-03 09:30:49 +0200

    Modificada la funcion implementada de la suma

--- sumRestMult.py
+++ sumRestMult.py
@@ -6,8 +6,8 @@ def sumar(a, b):
     return a + b
 
 if __name__ == "__main__":
-    num1 = float(input("Ingrese el primer número: "))
-    num2 = float(input("Ingrese el segundo número: "))
+    num1 = float(input("Pon el primer número: "))
+    num2 = float(input("Pon el segundo número: "))
     resultado = sumar(num1, num2)
     print("La suma es:", resultado)
 
